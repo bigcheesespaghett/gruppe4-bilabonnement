cafb7a65abc805d0c1a425108d7ac888729db333
heizetagram <davidlu1999@gmail.com>
2024-06-13 00:38:28 +0200

    Deleted CarModelRowMapper bean

--- src/main/java/com/example/gruppe4bilabonnement/services/Configuration.java
+++ src/main/java/com/example/gruppe4bilabonnement/services/Configuration.java
@@ -1,9 +1,7 @@
 package com.example.gruppe4bilabonnement.services;
 
 import com.example.gruppe4bilabonnement.models.Car;
-import com.example.gruppe4bilabonnement.models.CarModel;
 import com.example.gruppe4bilabonnement.models.Customer;
-import com.example.gruppe4bilabonnement.services.rowmappers.CarModelRowMapper;
 import com.example.gruppe4bilabonnement.services.rowmappers.CarRowMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
@@ -11,11 +9,6 @@ import org.springframework.jdbc.core.RowMapper;
 
 @org.springframework.context.annotation.Configuration
 public class Configuration {
-
-    @Bean
-    public RowMapper<CarModel> carModelRowMapper() {
-        return new CarModelRowMapper();
-    }
 
     @Bean
     public RowMapper<Car> carRowMapper() {
